ed5eeed19c419491142bb6788c224d789882eac3
Mukesh <mukeshchugh5@gmail.com>
2023-01-26 13:56:39 +0530

    format urls

--- todo_api/api/urls.py
+++ todo_api/api/urls.py
@@ -4,10 +4,10 @@ from api.views.task_view import add_task, get_all_tasks, update_task, delete_tas
 from api.views.user_view import register_user, validate_user_login
 
 urlpatterns = [
-    path('register_user', register_user, name="register_user"),
-    path('login', validate_user_login, name="validate_login"),
-    path('add_task', add_task, name="add_task"),
-    path('all_tasks', get_all_tasks, name="get_all_tasks"),
-    path('update/<int:task_id>', update_task, name='update_task'),
-    path('delete/<int:task_id>', delete_task, name='delete_task')
+    path('user/register', register_user, name="register_user"),
+    path('user/login', validate_user_login, name="validate_login"),
+    path('tasks/add', add_task, name="add_task"),
+    path('tasks/all', get_all_tasks, name="get_all_tasks"),
+    path('tasks/update/<int:task_id>', update_task, name='update_task'),
+    path('tasks/delete/<int:task_id>', delete_task, name='delete_task')
 ]
